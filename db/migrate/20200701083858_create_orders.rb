817fffaa0462674a17298ecd68f5d9f3b0f39e7b
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-01 17:46:03 +0900

    model

---
+++ db/migrate/20200701083858_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :end_user_id
+      t.integer :select
+      t.integer :toral_price
+      t.integer :send_price
+      t.integer :payment_way
+      t.string :postal_code
+      t.string :address
+      t.string :address_name
+
+      t.timestamps
+    end
+  end
+end
