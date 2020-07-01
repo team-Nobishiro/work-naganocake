62ce1f02855e35006dfd9d0b945d5b0f4ece0d4f
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-01 18:43:23 +0900

    update model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+  belongs_to :end_user
+	belongs_to :item
+  
+  
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,4 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :end_user
+  
 end
