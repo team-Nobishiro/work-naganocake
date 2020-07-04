c8195603282ea8b13c3ffdcd43b26a989e00581e
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:36:34 +0000

    トップページ/ヘッダー/フッター/Gem の追加

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
@@ -62,3 +63,13 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'jquery-rails'
+
+gem 'pry-byebug'
+
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
+
+gem 'ransack'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_07_02_141900) do
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "end_user_id"
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "end_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -33,27 +41,6 @@ ActiveRecord::Schema.define(version: 2020_07_02_141900) do
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
-  create_table "masters", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_masters_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_masters_on_reset_password_token", unique: true
-
-
-
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "item_id"
-    t.integer "end_user_id"
-    t.integer "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.boolean "is_valid"
@@ -70,6 +57,18 @@ ActiveRecord::Schema.define(version: 2020_07_02_141900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "genre_id"
+  end
+
+  create_table "masters", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_masters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_masters_on_reset_password_token", unique: true
   end
 
   create_table "order_items", force: :cascade do |t|
