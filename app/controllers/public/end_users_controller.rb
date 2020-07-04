4e9f566cc1fed1f522de7e37f2089b68ef47de93
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-04 17:10:43 +0900

    add action

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,2 +1,27 @@
 class Public::EndUsersController < ApplicationController
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+
+  def withdrawal
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def end_user_params
+    params.require(:end_user).permit(:email, :last_name, :first_name, :last_name_kana,
+                                     :first_name_kana, :phone_number, :postal_code, :address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 	root 'public/homes#top'
 	devise_for :master, controllers: {
   sessions: 'admin/masters/sessions'
-}
-devise_for :end_user
+  }
+  devise_for :end_user
 	get "public/homes/about" => "public/homes#about"
   
-  
+  # resources :public/end_users
+  get "public/end_users/show" => "public/end_users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
