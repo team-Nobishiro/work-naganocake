441ca1a499ca57b66f8245c49e23cb989cca41bd
vagrant <vagrant@localhost.localdomain>
2020-07-04 10:01:13 +0000

    update

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,2 +1,10 @@
 class Public::ShippingAddressesController < ApplicationController
+
+  def index
+  	@shipping_address_new = ShippingAddress.new
+  end
+
+  def edit
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-	root 'public/homes#top'
-	devise_for :master, controllers: {
-  sessions: 'admin/masters/sessions'
-}
-devise_for :end_user
+
+  root 'public/homes#top'
+
+  devise_for :master, controllers: {
+    sessions: 'admin/masters/sessions'
+  }
+
+  devise_for :end_users, controllers: {
+    :registrations => 'public/registrations'
+  }
+
 	get "public/homes/about" => "public/homes#about"
-  
+	get "public/shipping_address" => "public/shipping_addresses#index"  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
