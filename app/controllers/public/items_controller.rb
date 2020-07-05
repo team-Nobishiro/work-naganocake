c3d8a47d4598ffc1cbf020285562e3593fe0c053
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-05 21:37:28 +0900

    index show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
-    
+    @items = Item.all
   end
   
   def show
-    
+    @item = Item.find(params[:id])
   end
-  
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image, :not_taxprice)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   namespace :public do
     resource :end_users
     resources :shipping_addresses, except: [:new, :show]
+    resources :items, only: [:index, :show]
   end
   get "public/end_users/withdrawal" => "public/end_users#withdrawal"
   put "public/end_users/hide" => "public/end_users#hide", as: 'users_hide'
