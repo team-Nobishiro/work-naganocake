8a5f158a74f15a43d9d24c9bd08c2be75cafdd42
中村優作 <kickyuu5@gmail.com>
2020-07-05 16:26:08 +0900

    admin 会員機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,2 +1,36 @@
 class Public::EndUsersController < ApplicationController
+	before_action :authenticate_end_user!
+
+	def index
+	@end_users = EndUser.all
+	@end_user = EndUser.where(end_user_id: current_end_user.id)
+	
+	 
+	end
+
+	def show
+	@end_user = EndUser.where(end_user_id: current_end_user.id)
+
+	end
+
+	def edit
+	@end_user = EndUser.where(end_user_id: current_end_user.id)
+
+	end
+
+	before_action :authenticate_end_user!
+
+  def new
+    @end_user = current_end_user.id
+  end
+
+
+	
+
+    private
+    def end_user_params
+        params.require(:end_user).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :postal_code, :address)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
-	root 'public/homes#top'
-	devise_for :master, controllers: {
-  sessions: 'admin/masters/sessions'
-}
-devise_for :end_user, controllers: {
-  sessions: 'public/end_users/sessions',
-  registrations: 'public/end_users/registrations'
-}
+
 
 
   root 'public/homes#top'
@@ -19,10 +12,31 @@ devise_for :end_user, controllers: {
   devise_for :end_users, controllers: {
     :registrations => 'public/registrations'
   }
+ namespace :public do
+ resource :end_users
+ end
+
+namespace :admin do
+ resources :end_users
+ end
+
 
 
 	get "public/homes/about" => "public/homes#about"
 	get "public/shipping_address" => "public/shipping_addresses#index"  
+
+
+
+
   
+
+
+
+
+  
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
