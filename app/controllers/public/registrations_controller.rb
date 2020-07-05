8c9ff0e553e6cd2675353ee918c722d72cfa9133
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-05 16:22:36 +0900

    a

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,9 +59,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  protected
+  # protected
 
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
+  # def update_resource(resource, params)
+  #   resource.update_without_password(params)
+  # end
 end
