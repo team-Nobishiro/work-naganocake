8419bc697d0d5139553b4cf7c8dec090cc675457
vagrant <vagrant@localhost.localdomain>
2020-07-06 01:34:39 +0000

    Add admin_item_page

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,15 +1,21 @@
 class Admin::ItemsController < ApplicationController
 
   def new
+  	@item_new = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def index
+    @items = Item.all
   end
-  
+
   def show
+  	@item = Item.find(params[:id])
   end
 
   def edit
@@ -21,4 +27,9 @@ class Admin::ItemsController < ApplicationController
   def destroy
   end
 
+  private
+  def item_params
+  	params.require(:item).permit(:name, :introduction, :image, :not_taxprice, :is_sale_status, :genre_id)
+  end
+  	
 end
