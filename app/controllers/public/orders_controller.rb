19031f5eff438ad1933d349e7bcc38dde1eeacf4
vagrant <vagrant@localhost.localdomain>
2020-07-09 07:54:22 +0000

    Update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,7 @@ class Public::OrdersController < ApplicationController
     # @orders = Order.order('id DESC').limit(8)
     @end_user = current_end_user
     @orders = current_end_user.orders.order(created_at: :desc)
+    @toral_price = current_end_user.cart_total_price + @send_price
   end
   
   def show
@@ -33,7 +34,7 @@ class Public::OrdersController < ApplicationController
     elsif params[:select] == "my_address"
       # session[:address] ="〒" +current_end_user.postal_code+current_end_user.address+current_end_user.last_name+current_end_user.first_name
       session[:order][:address] = current_end_user.address
-      session[:order][:address_name] = current_end_user.address_name
+      session[:order][:address_name] = current_end_user.last_name + current_end_user.first_name
       session[:order][:postal_code] = current_end_user.postal_code
     
     else params[:select] == "new_address"
@@ -60,7 +61,10 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(session[:order])
     @order.select = 0
     @order.end_user_id = current_end_user.id
+    @order.toral_price = current_end_user.cart_total_price
     @order.save
+    @cart_items = current_end_user.cart_items
+    @cart_items.destroy_all
     redirect_to public_orders_thank_path
   end 
 
@@ -71,15 +75,9 @@ class Public::OrdersController < ApplicationController
   def thank
   end
 
-  def index
-  end
-
-  def show	
-  end
-
   private
   def order_params
-    params.require(:order).permit(:select, :payment_way, :postal_code, :address, :address_name)
+    params.require(:order).permit(:toral_price, :select, :payment_way, :postal_code, :address, :address_name)
   end
   def shipping_address_params
   	params.require(:shipping_address).permit(:postal_code, :address, :address_name)
