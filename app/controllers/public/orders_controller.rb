5de3e8ab7ab76743c67bd6988e96bc9b3e26bc99
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-09 14:36:41 +0900

    変更

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,16 +17,35 @@ class Public::OrdersController < ApplicationController
   	@shipping_addresses = current_end_user.shipping_addresses
   end
 
-  def create
+  def confirm
+    @order = Order.new
   	session[:payment_way] = params[:payment_way]
+    @send_price = 800
+    @toral_price = current_end_user.cart_total_price + @send_price
+    @end_user_id = current_end_user.id
+
     if params[:select] == "select_address"
-      session[:address] = params[:address]
+    @shipping_addresses = ShippingAddress.find(params[:address])
+    @postal_code = @shipping_addresses.postal_code
+    @address = @shipping_addresses.address
+    @address_name = @shipping_addresses.address_name
+ 
     elsif params[:select] == "my_address"
       session[:address] ="〒" +current_end_user.postal_code+current_end_user.address+current_end_user.last_name+current_end_user.first_name
+    @postal_code = current_end_user.postal_code
+    @address = current_end_user.address
+    @address_name = current_end_user.last_name+current_end_user.first_name
+
+    else params[:select] == "new_address"
+      session[:address] = params[:shipping_address]
+    @postal_code = params[:shipping_address][:postal_code]
+    @address = params[:shipping_address][:address]
+    @address_name = params[:shipping_address][:address_name]
+ 
     end
     
     if session[:address].present? && session[:payment_way].present?
-      redirect_to order_confirm_path
+      # redirect_to order_confirm_path
     else
 
       flash[:order_new] = "支払い方法または配送先を選択して下さい"
@@ -34,9 +53,9 @@ class Public::OrdersController < ApplicationController
     end
   end
 
-  def confirm
-  	@orders = current_end_user.orders
-  end
+  # def confirm
+  	# @orders = current_end_user.orders
+  # end
   
   def thank
   end
@@ -44,16 +63,12 @@ class Public::OrdersController < ApplicationController
   def index
   end
 
-  def show
-  	
+  def show	
   end
-
-  
 
   private
   def order_params
-    params.require(:order).permit(:end_user_id, :select, :toral_price, :send_price,
-                                  :payment_way, :postal_code, :address, :address_name)
+    params.require(:order).permit(:select, :payment_way, :postal_code, :address, :address_name)
   end
   def shipping_address_params
   	params.require(:shipping_address).permit(:postal_code, :address, :address_name)
