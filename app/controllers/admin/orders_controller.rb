b285f77517c6c6cce396c7d909a12e46758b25fe
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-11 18:21:59 +0900

    管理者　注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,17 @@ class Admin::OrdersController < ApplicationController
   end
   
   def show
+    @orders = Order.all.page(params[:page]).per(10)
     @order = Order.find(params[:id])
-    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    order = Order.find(params[:id])
+    if order.update(order_params)
+      redirect_to admin_order_path(order)
+    else
+      redirect_to admin_order_path(order)
+    end
   end
 
   private
