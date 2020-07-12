14290c4ba71419c77d9a8243213b97d90d54739b
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-12 13:17:09 +0900

    merge

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,12 +1,13 @@
 class Public::HomesController < ApplicationController
 
   def top
-  	@items = Item.all
+    @items = Item.all
+    @genres = Genre.all
   end
 
   def about
   	@end_users = EndUser.all
-	@end_user = EndUser.new    
+	  @end_user = EndUser.new    
   end
   
 end
