45f8c6ae8dc1baa872051af2700ebd04009523a5
中村優作 <kickyuu5@gmail.com>
2020-07-13 16:01:36 +0900

    コミット

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.assets.enabled = true
   end
 end
